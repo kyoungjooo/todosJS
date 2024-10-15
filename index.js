e95616c9b0ec56ffb544b1e8f70c1ae991621b88
leekjoo <leekjoo1008@gmail.com>
2024-10-15 19:15:33 +0900

    refector: 함수 수정

--- index.js
+++ index.js
@@ -1,37 +1,43 @@
-const todos = [
+let todos = [
   {
-    id: 1,
+    id: 0,
     text: "집 청소하기",
     status: "notCompleted",
   },
   {
-    id: 2,
+    id: 1,
     text: "공부하기",
     status: "notCompleted",
   },
   {
-    id: 3,
+    id: 2,
     text: "산책하기",
     status: "notCompleted",
   },
   {
-    id: 4,
+    id: 3,
     text: "양치하기",
     status: "notCompleted",
   },
 ];
 
 const todo = document.querySelector(".todoList > ul");
+const tabs = document.querySelectorAll("header>button");
 
 //카드를 감싸주는 li 생성
 function createTodoCard() {
+  tabs.forEach((tab) => {
+    tab.addEventListener("click", (e) => handleShowTodos(e));
+  });
   todos.forEach((el) => {
     const card = document.createElement("li");
     card.className = "todo";
     const checkBox = document.createElement("input");
     const label = document.createElement("label");
     const textWrap = document.createElement("span");
+    textWrap.className = "input-wrap";
     const remove = document.createElement("button");
+    remove.addEventListener("click", (e) => removeCurrnetTodo(e));
     card.append(textWrap);
     textWrap.append(checkBox);
     textWrap.append(label);
@@ -44,42 +50,35 @@ function createTodoCard() {
     checkBox.setAttribute("type", "checkbox");
     checkBox.setAttribute("id", el.id);
     checkBox.setAttribute("value", el.status);
+    checkBox.addEventListener("change", (e) => setInputCheckedValue(e));
     todo.append(card);
   });
 }
 createTodoCard();
 
 //input의 상태가 변경될때마다 체크되면 value를 complete로 그렇지 않으면 notComplete로 업데이트
-
-const checkBoxes = document.querySelectorAll(".todoList > ul > li > input");
-
-for (const box of checkBoxes) {
-  box.addEventListener("change", (e) => {
-    const currentBox = e.target;
-    currentBox.checked
-      ? currentBox.setAttribute("value", "Completed")
-      : currentBox.setAttribute("value", "notCompleted");
-  });
-}
+const setInputCheckedValue = (e) => {
+  const currentBox = e.target;
+  currentBox.checked
+    ? currentBox.setAttribute("value", "Completed")
+    : currentBox.setAttribute("value", "notCompleted");
+};
 
 //header 버튼 안에있는 버튼이 클릭되면 일치하는 todo만 보여주기
-const tabs = document.querySelectorAll("header>button");
-tabs.forEach((tab) => {
-  tab.addEventListener("click", (e) => {
-    console.log(e.target.value);
-    const currentTab = e.target.value;
-    checkBoxes.forEach((el) => {
-      const block = (el.parentNode.style.display = "block");
-      if (currentTab === "All") {
-        block;
-      } else {
-        currentTab === el.value
-          ? block
-          : (el.parentNode.style.display = "none");
-      }
-    });
+const handleShowTodos = (e) => {
+  const currentTab = e.target.value;
+  const checkBoxes = document.querySelectorAll(".input-wrap>input");
+  checkBoxes.forEach((el) => {
+    const block = (el.parentNode.parentNode.style.display = "block");
+    if (currentTab === "All") {
+      block;
+    } else {
+      currentTab === el.value
+        ? block
+        : (el.parentNode.parentNode.style.display = "none");
+    }
   });
-});
+};
 
 //input text에 글자 입력하면 todos에 목록을 추가한다.
 const submit = document.querySelector("form");
@@ -88,8 +87,10 @@ const textInput = document.querySelector(".inputTodo > input");
 
 function handleFormSubmit(e) {
   e.preventDefault();
+  console.log("전송");
   const text = textInput.value;
-  if (text.trim().length < 1) return;
+  console.log(text.length);
+  if (text.trim().length === 0) return;
   const newTodo = { id: todos.length + 1, text, status: "notCompleted" };
   todos.push(newTodo);
   todo.innerHTML = "";
@@ -98,8 +99,24 @@ function handleFormSubmit(e) {
 }
 textInput.addEventListener("input", addTextInput);
 function addTextInput(e) {
+  console.log("입력", e);
   const newTodo = e.target.value;
 }
 
+// 삭제 버튼을 누르면 해당 todo를 제거해준다.
 const removeBtns = document.querySelectorAll(".todo .remove");
-console.log(removeBtns);
+
+const removeCurrnetTodo = (e) => {
+  console.log("삭제", e);
+  const removeTarget = Number(e.target.value);
+  console.log(removeTarget);
+  todos = todos.filter((todo) => {
+    return todo.id !== removeTarget;
+  });
+  todo.innerHTML = "";
+  createTodoCard();
+};
+
+const handleKeyPress = (e) => {
+  console.log(e.key);
+};
